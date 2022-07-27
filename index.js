df8bf487e79b5f36ea0999c4778bee9b4fa9a4f6
PedroLucasLopes <pedrolopesparaguai@gmail.com>
2022-07-26 23:07:11 -0300

    Fixing this shit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const twitter = new Twit({
     consumer_key: process.env.APPLICATION_CONSUMER_KEY,
     consumer_secret: process.env.APPLICATION_CONSUMER_SECRET,
     access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
 
 //Function to log errors from requests
@@ -14,15 +14,11 @@ function responseCallback(err, data, response) {
     console.log(err);
 }
 
-let media;
-
 //Calling the stream and tracking tweets
-const stream = twitter.stream('statuses/filter', { track: `@sonoplasta_bot ${media}` });
+const stream = twitter.stream('statuses/filter', { track: `@sonoplasta_bot` });
 
 // event handler
 stream.on('tweet', tweet => {
     // retweet
     twitter.post('statuses/retweet/:id', { id: tweet.id_str }, responseCallback);
-    // like
-    twitter.post('favorites/create', { id: tweet.id_str }, responseCallback);
 });
